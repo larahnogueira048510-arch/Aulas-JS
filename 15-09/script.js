6856ec6d9b6f4ccebb47884f1fc2f9786e54f890
Larah <larah.nogueira048510@gmail.com>
2026-09-15 11:38:44 -0300

    adicionada pasta 15-09 - variaveis

---
+++ 15-09/script.js
@@ -0,0 +1,44 @@
+// let Eilish = " Cantora "
+// let idade = 24;
+
+// let finneas = " Cantor "
+// var nome = " Billie "
+// nome = " o'connell"
+
+// const nome = " Billie "
+// nome = " O' connell "
+
+
+// idade = 25;
+
+
+// console.log("A Eilish é " + Eilish + " e ela tem " + idade + " anos ");
+// console.log("O finneas é " + finneas);
+//  crie 4 variaveis contendo: nome, sobrenome; idade e curso 
+// Exiba no console em uma unica frase
+
+let nome = " Larah ";
+let sobrenome = " Nogueira Nunes ";
+let idade = " 16 ";
+let curso = " javasScript ";
+
+console.log(" Eu sou a " + nome + sobrenome + " tenho "  + idade + " Estou fazendo curso de " + curso);
+
+
+// let nome = " Larah "; // string
+// let peso = 60; // int
+// let altura = 1.68; // float 
+// let funcionario = false;//bool - true == verdadeiro ou false == falso
+
+// let cidade = null;
+// let estado;
+// console.log(typeof nome);
+// console.log(typeof peso);
+// console.log(typeof altura);
+// console.log(typeof funcionario);
+
+console.log(" A variavel nome tem " + nome + " e é do tipo " + typeof nome); 
+console.log(" A variavel sobrenome tem " + sobrenome + " e é do tipo " + typeof sobrenome); 
+console.log(" A variavel idade tem " + idade + " e é do tipo " + typeof idade); 
+console.log(" A variavel curso tem " + curso + " e é do tipo " + typeof curso); 
+
