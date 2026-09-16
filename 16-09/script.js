c36b29e53e6e59f69409a3a176fffcb3a4363c5d
Larah <larah.nogueira048510@gmail.com>
2026-09-16 11:56:29 -0300

    criação da pasta 16-09 e estrutura condicional

---
+++ 16-09/script.js
@@ -0,0 +1,126 @@
+// operadora aritimeticos
+// + soma
+// - subtração
+// * multiplicação
+// / divisão
+// % sobra da divisão 
+
+// let n1 = 3;
+// let n2 = 9;
+
+// console.log(n1 + n2);
+// console.log(n1 - n2);
+// console.log(n1 * n2);
+// console.log(n1 / n2);
+// console.log(n1 % n2);
+// console.log( (n2 - n1) * 2);
+// console.log("palavra" + n1);
+
+// crie 3 variaveis, cada uma contendo um numero, 
+// some as 3 e tire a média, exiba o resultado no console
+
+// let n1 = 9 
+// let n2 = 6
+// let n3 = 7
+
+// console.log( (n1 + n2 + n3) /3);
+
+// let n1 = 9;
+// let n2 = 6;
+// let n3 = 7;
+
+// console.log(" A média de " + n1, n2, n3 + " é " + ( n1 + n2 + n3 ) / 3)
+
+// > maior que 
+// < menor que 
+// == igual
+// >= maior ou igual
+// <= menor ou igual
+// != é diferente 
+
+// let idade = 18
+
+// console.log(idade > 18);
+// console.log(idade < 18);
+// console.log(idade == 18);
+// console.log(idade >= 18);
+// console.log(idade <= 18);
+// console.log(idade != 18);
+
+// console.log(idade === 18); // comparação de tipo de variaveis
+
+// let idade = 18;
+// let habilitado = true;
+// console.log(idade <= 18 && habilitado == true);
+// V && V = V
+// V && F = V
+// F && V = F
+// F && F = F
+
+// console.log(idade <= 18 || habilitado == true);
+// V || V = V
+// V || F = V
+// F || V = F
+// F || F = F
+
+// ! operador NOT "inverte"
+// console.log(!habilitado);
+ 
+
+// se idade for igual a 18 
+     //faça isso
+
+//se não
+   //faça isso
+
+//    let habilitado = false
+
+// if (habilitado == true) {// se 
+//     console.log("pode dirigir");
+
+// } else {//se não
+//     console.log("NÃO pode dirigir");
+    
+// }
+
+// let idade = 16;
+
+// // if (idade >= 18) {
+// if (idade >= 17) {
+//     console.log("Maior de idade!" );
+
+// } else {
+//     console.log("Menor de idade");
+// }
+
+// crie um sistema que compare o valor de uma variavel
+// senha SE for igual a 1234 escreva no console 
+//" acesso liberado" SE NÃO escreva "acesso bloqueado"
+
+// let senha = 0521
+
+// if (senha == 1234 ) {
+//     console.log(" ACESSO LIBERADO ")
+
+// } else {
+//     console.log(" ACESSO NEGADO ")
+// }
+
+// let nota = 4
+
+// if (nota >= 6) {
+//     console.log("aprovado")
+    
+// } else {
+//     console.log("reprovado")
+// }
+
+let valor = 350;
+let deconto = 20;
+
+if (valor >= 500) {
+    console.log(valor - 20 );
+} else {
+    console.log("SEM DESCONTO");
+}
+
