5e98649c1d4c8da8c5319b7efad3caeb423edcd2
Larah <larah.nogueira048510@gmail.com>
2026-09-23 11:54:10 -0300

    aula 23/09 - estrutura de repetição

---
+++ 23-09/script.js
@@ -0,0 +1,150 @@
+// let idade = 18
+
+// if (iadade >= 18 ) {
+//     console.log("maior de idade");
+// } else {
+//     console.log("menor de idade");
+// }
+
+// let media = 7; 
+
+// if (média >= 7) {
+//     console.log("aprovado")
+
+// } else if (média == 5 || media == 6) {
+//     console.log("recuperação");
+// }else{
+//     console.log("Reprovado")
+// }
+
+// ----------ESTRUTURAS DE REPETIÇÃO------
+
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("5");
+// console.log("6");
+// console.log("7");
+// console.log("8");
+// console.log("9");
+// console.log("10");
+
+//FOR
+// for (variavel; condição; incremento) {
+
+// }
+
+//INCREMENTO
+// for (let index = 0; index < 10; index--) {
+//     console.log(index);
+    
+    
+// }
+//DECREMENTANDO
+// for (let index = 10; index >= 0; index--) {
+//     console.log(index);
+    
+// }
+//INCREMENTO PERSONALIZADO
+// for (let index = 10; index >= 0; index-=5) {
+//     console.log(index);
+    
+// }
+
+// for (let index = 10; index <=20; index++) {
+//     console.log(index);
+// }
+
+// for (let index = 5 ; index <=50; index+=5) {
+//     console.log(index);
+// }
+
+// for (let index = 10; index >= 0; index--) {
+//     console.log(index);
+// }
+ 
+//     console.log("contagem finalizada");
+
+//----------WHILE
+
+// let contador = 0;
+// while (contador <= 10) {
+    
+
+// console.log(contador);
+
+//     contador++;
+// }
+
+
+// let contador = 10;
+// while (contador <= 20) {
+
+//     console.log(contador);
+
+//     contador++;
+    
+// }
+
+// let contador = 2;
+// while (contador <= 20) {
+    
+//     console.log(contador);
+
+//     contador+=2;
+// }
+
+//---------- DO ----WHILE
+// let n = 10;
+// while (n < 5) {
+//     console.log("WHILE");
+// }
+
+// do {
+//     console.log("DO....WHILE");
+
+// } while (n < 5); 
+    
+// let contador = 0;
+// do{
+//     console.log(contador);
+
+//     contador++;
+
+// } while (contador <= 10) 
+
+// let contador = 10;
+// do{
+//     console.log(contador);
+
+//     contador++;
+
+// }while (contador <= 100);
+
+// let contador = 10;
+// do{
+//     console.log(contador);
+
+//     contador--;
+
+// } while (contador >= 1)
+
+
+// ----------ARRAY (LISTA)-----------
+// let neme1 = "Levi";
+// let nome2 = "Duda";
+// let nome3 = "Gustavo";
+// let nome4 = "Bernardo";
+
+// //              0       1        2          3
+// let nomes = ["Levi", "Duda"," Gustavo","Bernardo"];//Criação do Array/lista
+
+// console.log(nomes); // mostra a lista completa na mesma linha
+
+// console.log(nomes[1]);// mostra o item da posição mencionada entre colchetes
+
+// nome4 = "Ana";
+// nomes[3]= "Ana"; //altero o valo da posição especificada
+
+// console.log(nomes.length);//mostra o tamanho do Array
